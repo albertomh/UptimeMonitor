5cfd49c229d88e169bba4dccb0251d9e838387f2
Alberto Morón Hernández <albertomh@users.noreply.github.com>
2026-09-03 08:15:44 +0100

    refactor: use semantic Terraform resource names (#30)

--- outputs.tf
+++ outputs.tf
@@ -1,16 +1,16 @@
 output "worker_script_name" {
   description = "Name of the Cloudflare Worker script."
-  value       = try(cloudflare_workers_script.this[0].script_name, null)
+  value       = try(cloudflare_workers_script.worker[0].script_name, null)
 }
 
 output "d1_database_id" {
   description = "ID of the D1 database used by the Worker."
-  value       = try(cloudflare_d1_database.this[0].id, null)
+  value       = try(cloudflare_d1_database.database[0].id, null)
 }
 
 output "d1_database_name" {
   description = "Name of the D1 database used by the Worker."
-  value       = try(cloudflare_d1_database.this[0].name, null)
+  value       = try(cloudflare_d1_database.database[0].name, null)
 }
 
 output "status_hostname" {
